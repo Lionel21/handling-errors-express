f0cd8ae037c871810399939b7a6521cc95733198
Lionel Delamare <ldelamare@entreloups.com>
2021-10-24 14:52:06 +0200

    refactoring input error message

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express, {NextFunction, Request, Response} from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import wilderRouter from './routes/wilder';
@@ -8,24 +8,24 @@ const app = express();
 
 // Database
 mongoose
-  .connect('mongodb://127.0.0.1:27017/wilderdb', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    autoIndex: true,
-  })
-  // eslint-disable-next-line no-console
-  .then(() => console.log('Connected to database'))
-  // eslint-disable-next-line no-console
-  .catch((err: Error) => console.log(err));
+    .connect('mongodb://127.0.0.1:27017/wilderdb', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        autoIndex: true,
+    })
+    // eslint-disable-next-line no-console
+    .then(() => console.log('Connected to database'))
+    // eslint-disable-next-line no-console
+    .catch((err: Error) => console.log(err));
 
 // Middleware
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Hello World');
+    res.send('Hello World');
 });
 
 app.use(wilderRouter);
@@ -33,37 +33,41 @@ app.use(wilderRouter);
 
 // TODO : handle not found in the error middleware
 app.get('*', (req, res) => {
-  res.status(404);
-  res.send({ success: false, message: 'Wrong adress' });
+    res.status(404);
+    res.send({success: false, message: 'Wrong adress'});
 });
 
 interface MongoError extends Error {
-  code: number;
+    code: number;
 }
 
 function isMongoError(error: Error): error is MongoError {
-  return error.name === 'MongoError';
+    return error.name === 'MongoError';
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: Error, req: Request, res: Response, _next: NextFunction) => {
-  // TODO : do not rely on mongo error
-  if (isMongoError(error)) {
-    switch (error.code) {
-      case 11000:
-        res.status(400);
-        res.json({ success: false, message: 'The name is already used' });
-        break;
-      default:
-        res.status(400);
-        res.json({ success: false, message: 'An error occured' });
+    // TODO : do not rely on mongo error
+    if (isMongoError(error)) {
+        switch (error.code) {
+            case 11000:
+                res.status(400);
+                res.json({success: false, message: 'The name is already used'});
+                break;
+            default:
+                res.status(400);
+                res.json({success: false, message: 'An error occured'});
+        }
     }
-  }
-  // TODO : use a better validation method (express-validator)
-
-  if (error instanceof InputError) {
-    res.json({ success: false, message: 'An input error occured', error });
-  }
+    // TODO : use a better validation method (express-validator)
+    if (error instanceof InputError) {
+        res.status(400).json({
+            status: 400,
+            errors: error.validationErrors.map(
+                ({msg}) => msg
+            )
+        });
+    }
 });
 
 // Start Server
